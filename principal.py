f695081293308e959b89f9783407a9617c727350
Inaki Ospitaletche <ospita.lorenzo@gmail.com>
2021-04-09 23:19:57 -0300

    Suspende

--- principal.py
+++ principal.py
@@ -3,4 +3,6 @@ UT SERVIAM
 Este es un blog sobre el Serviam.
 En este espacio voy a comentar todos los partidos del 2021.
 
-El primer partido fue contra young y perdimos 1-0
+El primer partido fue contra young y perdimos 1-0.and
+
+Luego se suspendio la liga.
