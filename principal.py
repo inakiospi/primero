5a11517e3092b19749d991d41a0d77838480abe3
Inaki Ospitaletche <ospita.lorenzo@gmail.com>
2021-04-10 20:39:24 -0300

    por covid

--- principal.py
+++ principal.py
@@ -5,4 +5,4 @@ En este espacio voy a comentar todos los partidos del 2021.
 
 El primer partido fue contra young y perdimos 1-0.and
 
-Luego se suspendio la liga.
+Luego se suspendio la liga debido al covid.
